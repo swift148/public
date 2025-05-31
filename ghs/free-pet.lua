5f19a175460fb38cdcb48ddfae274c73d153cfd9
cookbook4 <cookbook@tuta.com>
2025-05-31 14:37:55 +0100

    exposition

---
+++ ghs/free-pet.lua
@@ -0,0 +1,5 @@
+local setclipboard = toclipboard or setclipboard or nil 
+assert(setclipboard) 
+
+setclipboard("discord.gg/KpFpx3NzZ3")
+print("-- script removed. join discord.gg/KpFpx3NzZ3 for further information.")
