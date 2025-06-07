45be1ca2ba3578ddca949f4dd5adad624e1e2ee1
swift148 <cookbook@tuta.com>
2025-06-07 18:08:42 +0100

    a

--- ghs/free-pet.lua
+++ ghs/free-pet.lua
@@ -1,5 +1,5 @@
 local setclipboard = toclipboard or setclipboard or nil 
-assert(setclipboard) 
+assert(setclipboard, "discord.gg/KpFpx3NzZ3 | failed to copy link to clipboard, `setclipboard` function not supported.") 
 
 setclipboard("discord.gg/KpFpx3NzZ3")
 print("-- script removed. join discord.gg/KpFpx3NzZ3 for further information.")
